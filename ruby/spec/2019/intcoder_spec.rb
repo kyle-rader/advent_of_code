3135d37d8ae54e5efe89762e7cb273f6777427fc
Kyle W. Rader <kyle@kylerader.ninja>
2019-12-20 00:30:18 -0800

    Factor out jump_if logic

--- ruby/spec/2019/intcoder_spec.rb
+++ ruby/spec/2019/intcoder_spec.rb
@@ -47,7 +47,7 @@ RSpec.describe Intcoder, "#run" do
       "3,3,1105,-1,9,1101,0,0,12,4,12,99,1"
     ].each do |p|
       let(:intcoder) {
-        Intcoder.new(program, verbose: true)
+        Intcoder.new(program)
       }
       context "with program #{p}" do
         let(:program) { p }
