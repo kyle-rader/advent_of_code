b8bfc977c1d2b5a30612af63dd6e34c47eb222ad
Kyle W. Rader <kyle@kylerader.ninja>
2019-12-20 00:33:04 -0800

    Add large exmaple

--- ruby/spec/2019/intcoder_spec.rb
+++ ruby/spec/2019/intcoder_spec.rb
@@ -108,6 +108,22 @@ RSpec.describe Intcoder, "#run" do
       end
     end
   end
+
+  describe "opcodes 5-8" do
+    let(:program) { "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99" }
+    context "less than 8" do
+      let(:input) { [3] }
+      it { is_expected.to eq [999] }
+    end
+    context "equal to 8" do
+      let(:input) { [8] }
+      it { is_expected.to eq [1000] }
+    end
+    context "grater than 8" do
+      let(:input) { [15] }
+      it { is_expected.to eq [1001] }
+    end
+  end
 end
 
 RSpec.describe Intcoder, "self.get_modes" do
