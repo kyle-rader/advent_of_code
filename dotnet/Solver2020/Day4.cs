91a05324ede236dcea79fc44087ac2c011808b1d
Kyle Rader (HE|HIM) <kyrader@microsoft.com>
2020-12-03 21:18:23 -0800

    Solve day 4 part 1

--- dotnet/Solver2020/Day4.cs
+++ dotnet/Solver2020/Day4.cs
@@ -1,5 +1,7 @@
 ﻿using SolverBase;
 
+using System.Collections;
+using System.Collections.Generic;
 using System.IO.Abstractions;
 using System.Linq;
 
@@ -11,12 +13,41 @@ namespace Solver2020
 
         public override string Solve(string inputFile)
         {
-            return null;
+            var inputRaw = fileSystem.File.ReadAllText(inputFile);
+            var splits = inputRaw.Split(new[] { "\r\n\r\n" }, System.StringSplitOptions.RemoveEmptyEntries);
+
+            return splits.Select(data => new Passport(data)).Where(p => p.Valid()).Count().ToString();
         }
 
         public override string Solve2(string inputFile)
         {
             return null;
+        }
+
+        public class Passport
+        {
+            static readonly HashSet<string> RequiredFields = new HashSet<string>()
+            {
+                "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", // "cid" optional
+            };
+
+            IDictionary<string, string> fields;
+
+            public Passport(string input)
+            {
+                fields = new Dictionary<string, string>();
+
+                foreach (string pair in input.Split(new[] { "\r\n", " " }, System.StringSplitOptions.RemoveEmptyEntries))
+                {
+                    var parts = pair.Split(new[] { ':' }, System.StringSplitOptions.RemoveEmptyEntries);
+                    fields[parts[0]] = parts[1];
+                }
+            }
+
+            public bool Valid()
+            {
+                return RequiredFields.All(field => fields.ContainsKey(field));
+            }
         }
     }
 }
