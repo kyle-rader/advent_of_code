61b4b95f999253b8c7b008e56301a40115ae0a0e
Kyle Rader (HE|HIM) <kyrader@microsoft.com>
2020-12-03 18:07:46 -0800

    Solve day 3 part 1

---
+++ dotnet/Solver2020/Day3.cs
@@ -0,0 +1,35 @@
+﻿using SolverBase;
+
+using System.IO.Abstractions;
+using System.Linq;
+
+namespace Solver2020
+{
+    public class Day3 : Base
+    {
+        public Day3(IFileSystem fileSystem) : base(fileSystem) { }
+
+        public override string Solve(string inputFile)
+        {
+            char[][] map = InputItems(inputFile).Select(line => line.ToCharArray()).ToArray();
+
+            return TreesOnPath(map, 3).ToString();
+        }
+
+        public override string Solve2(string inputFile)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static int TreesOnPath(char[][] map, int reach)
+        {
+            int trees = 0;
+            int xMod = map[0].Length;
+            for (int i = 0; i < map.Length; i++)
+            {
+                trees += map[i][(i * reach) % xMod] == '#' ? 1 : 0;
+            }
+            return trees;
+        }
+    }
+}
---
+++ dotnet/Solver2020Tests/Day3Test.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+
+using NUnit.Framework;
+
+using Solver2020;
+
+namespace Solver2020Tests
+{
+    public class Day3Test : TestBase
+    {
+        [SetUp]
+        public void Setup()
+        {
+            SetInput(@"..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#");
+
+            solver = new Day3(fileSystem);
+        }
+
+        [Test]
+        public void Part1Example()
+        {
+            Solve1Int().Should().Be(7);
+        }
+
+        [Test]
+        public void Part2Example()
+        {
+            //Solve2Int().Should().Be(0);
+        }
+    }
+}
