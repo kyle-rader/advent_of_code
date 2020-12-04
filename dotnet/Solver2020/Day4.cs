7be658138c5c9393ba24cd628a80596652d001ec
Kyle Rader (HE|HIM) <kyrader@microsoft.com>
2020-12-03 20:59:40 -0800

    Setup day4

---
+++ dotnet/Solver2020/Day4.cs
@@ -0,0 +1,22 @@
+﻿using SolverBase;
+
+using System.IO.Abstractions;
+using System.Linq;
+
+namespace Solver2020
+{
+    public class Day4 : Base
+    {
+        public Day4(IFileSystem fileSystem) : base(fileSystem) { }
+
+        public override string Solve(string inputFile)
+        {
+            return null;
+        }
+
+        public override string Solve2(string inputFile)
+        {
+            return null;
+        }
+    }
+}
---
+++ dotnet/Solver2020Tests/Day4Test.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+
+using NUnit.Framework;
+
+using Solver2020;
+
+namespace Solver2020Tests
+{
+    public class Day4Test : TestBase
+    {
+        [SetUp]
+        public void Setup()
+        {
+            SetInput(@"");
+
+            solver = new Day4(fileSystem);
+        }
+
+        [Test]
+        public void Part1Example()
+        {
+            //Solve1Int().Should().Be(0);
+        }
+
+        [Test]
+        public void Part2Example()
+        {
+            //Solve2Double().Should().Be(0);
+        }
+    }
+}
