9b45dd3d7f38d6054cbd902ac4acdae48ed54245
Kyle Rader <kyle@kylerader.ninja>
2020-12-05 10:44:52 -0800

    Solve day 5 part 1

---
+++ dotnet/Solver2020/2020/Day5.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO.Abstractions;
+using System.Linq;
+
+namespace Solver
+{
+    public class Day5 : Base
+    {
+        public Day5(IFileSystem fileSystem) : base(fileSystem)
+        {
+        }
+
+        public override string Solve(string inputFile)
+        {
+            return InputItemsStrings(inputFile).Select(x => new BoardingPass(x).Id).Max().ToString();
+        }
+
+        public override string Solve2(string inputFile)
+        {
+            return null;
+        }
+
+        public class BoardingPass
+        {
+            public int Row;
+            public int Col;
+            public int Id;
+
+            public BoardingPass(string input)
+            {
+                int min = 0, max = 127;
+                for (int i = 0; i < 6; i++)
+                {
+                    if (input[i] == 'F')
+                        max = max - (max - min) / 2;
+                    else if (input[i] == 'B')
+                        min = min + (max - min) / 2;
+                }
+                Row = BinarySearch(input.Substring(0, 7), 'F', 'B', 0, 127);
+                Col = BinarySearch(input.Substring(7), 'L', 'R', 0, 7);
+                Id = 8 * Row + Col;
+            }
+
+            public static int BinarySearch(string input, char low, char high, int min, int max)
+            {
+                var n = input.Length;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    if (input[i] == low)
+                        max = max - 1 - (max - min) / 2;
+                    else if (input[i] == high)
+                        min = min + (int)Math.Ceiling((max - min) / 2.0);
+                }
+                return input[n - 1] == low ? min : max;
+            }
+        }
+    }
+}
---
+++ dotnet/Solver2020Tests/2020/Day5Test.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+
+using NUnit.Framework;
+
+using Solver;
+
+namespace Tester
+{
+    public class Day5Test : TestBase
+    {
+        [SetUp]
+        public void Setup()
+        {
+            SetInput(@"FBFBBFFRLR
+BFFFBBFRRR
+FFFBBBFRRR
+BBFFBBFRLL");
+            solver = new Day5(fileSystem);
+        }
+
+        [Test]
+        public void Part1Example()
+        {
+            Solve1Int().Should().Be(820);
+        }
+
+        [Test]
+        public void Part2ExampleBadPassports()
+        {
+            SetInput(@"");
+            Solve2Int().Should().Be(0);
+        }
+
+        [TestCase("FBFBBFFRLR", 44, 5, 357)]
+        [TestCase("BFFFBBFRRR", 70, 7, 567)]
+        [TestCase("FFFBBBFRRR", 14, 7, 119)]
+        [TestCase("BBFFBBFRLL", 102, 4, 820)]
+        [TestCase("FFFFFFFRRR", 0, 7, 7)]
+        [TestCase("FFFFFFFLLL", 0, 0, 0)]
+        public void BoardingPassCanBeParsed(string input, int row, int col, int id)
+        {
+            var subject = new Day5.BoardingPass(input);
+            subject.Row.Should().Be(row);
+            subject.Col.Should().Be(col);
+            subject.Id.Should().Be(id);
+        }
+    }
+}
