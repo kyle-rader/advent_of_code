522c0f148784d9e2be3b02c141c71c6662c12c62
Kyle Rader <kylewrader@gmail.com>
2020-12-05 19:33:02 -0800

    Solve 2015 day 1

---
+++ dotnet/Solver/2015/Day1.cs
@@ -0,0 +1,36 @@
+using System.IO.Abstractions;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Solver._2015
+{
+    public class Day1 : Base
+    {
+        public Day1(IFileSystem fileSystem) : base(fileSystem) { }
+
+        public override string Solve(string inputFile)
+        {
+            return Input(inputFile)
+                .ToCharArray()
+                .Aggregate(
+                    0,
+                    (acc, c) => acc + (c == '(' ? 1 : -1),
+                    (final) => final.ToString());
+        }
+
+        public override string Solve2(string inputFile)
+        {
+            int floor = 0, i = 1;
+            foreach (var c in Input(inputFile).ToCharArray())
+            {
+                floor += c == '(' ? 1 : -1;
+                if (floor < 0) break;
+                i++;
+            }
+            return i.ToString();
+        }
+    }
+}
