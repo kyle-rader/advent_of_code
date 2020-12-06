c72e842deecbb854cbc24e2a4f96f9f42d47e75a
Kyle Rader <kylewrader@gmail.com>
2020-12-05 18:15:56 -0800

    Gen 2020 day 6

---
+++ dotnet/Solver/2020/Day6.cs
@@ -0,0 +1,26 @@
+using System.IO.Abstractions;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Solver._2020
+{
+    public class Day6 : Base
+    {
+        public Day6(IFileSystem fileSystem) : base(fileSystem) { }
+
+        public override string Solve(string inputFile)
+        {
+            var input = Input(inputFile);
+            return null;
+        }
+
+        public override string Solve2(string inputFile)
+        {
+            var input = Input(inputFile);
+            return null;
+        }
+    }
+}
---
+++ dotnet/Tester/2020/Day6Test.cs
@@ -0,0 +1,32 @@
+using FluentAssertions;
+
+using NUnit.Framework;
+
+using Solver._2020;
+
+namespace Tester._2020
+{
+    public class Day6Test : TestBase
+    {
+        [SetUp]
+        public void Setup()
+        {
+            SetInput(@"
+");
+
+            solver = new Day6(fileSystem);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            //Solve1Int().Should().Be(0);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            //Solve2Int().Should().Be(0);
+        }
+    }
+}
