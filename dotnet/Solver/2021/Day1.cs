73a8aa8e91722025a5cdbfde244a5bb0ca7a3ce0
Kyle Rader <kylewrader@gmail.com>
2021-12-02 18:30:28 -0800

    dotnet 2021 day 1

---
+++ dotnet/Solver/2021/Day1.cs
@@ -0,0 +1,47 @@
+using System.IO.Abstractions;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Solver._2021
+{
+    public class Day1 : Base
+    {
+        public Day1(IFileSystem fileSystem) : base(fileSystem) { }
+
+        private int CountIncreases(int[] items)
+        {
+            int increases = 0;
+            for (int i = 1; i < items.Length; i++)
+            {
+                if (items[i] > items[i - 1])
+                    increases++;
+            }
+            return increases;
+        }
+
+        private IEnumerable<int> SlidingWindowSums(int[] depths)
+        {
+            int n = depths.Length - 2;
+            for (int i = 0; i < n; i++)
+            {
+                yield return depths[i] + depths[i + 1] + depths[i + 2];
+            }
+        }
+
+        public override double Solve(string inputFile)
+        {
+            int[] depths = InputItemsInts(inputFile).ToArray();
+            return CountIncreases(depths);
+        }
+
+        public override double Solve2(string inputFile)
+        {
+            int[] depths = InputItemsInts(inputFile).ToArray();
+            var sums = SlidingWindowSums(depths);
+            return CountIncreases(sums.ToArray());
+        }
+    }
+}
