024f16746b419396b4967e4aaf56b3f664fc29b9
Kyle Rader <kylewrader@gmail.com>
2021-12-02 20:05:04 -0800

    dotnet 2021 day 2

---
+++ dotnet/Solver/2021/Day2.cs
@@ -0,0 +1,83 @@
+using System.IO.Abstractions;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Solver._2021
+{
+    public class Day2 : Base
+    {
+        public Day2(IFileSystem fileSystem) : base(fileSystem) { }
+
+        private struct Position
+        {
+            public int Depth;
+            public int PositionHorizontal;
+            public int Aim;
+
+            public void Add(Position other)
+            {
+                Depth += other.Depth;
+                PositionHorizontal += other.PositionHorizontal;
+            }
+        }
+
+        private IEnumerable<Position> Steps(IEnumerable<string> stepsRaw)
+        {
+            foreach(var line in stepsRaw)
+            {
+                var parts = line.Split();
+                int value = int.Parse(parts[1]);
+                if (parts[0] == "forward")
+                {
+                    yield return new Position() { PositionHorizontal = value };
+                }
+                if (parts[0] == "up")
+                {
+                    yield return new Position() { Depth = -value };
+                }
+                if (parts[0] == "down")
+                {
+                    yield return new Position { Depth = value };
+                }
+            }
+        }
+
+        public override double Solve(string inputFile)
+        {
+            var input = InputItems(inputFile);
+            Position position = new Position();
+            foreach (var step in Steps(input))
+                position.Add(step);
+
+            return position.Depth * position.PositionHorizontal;
+        }
+
+        public override double Solve2(string inputFile)
+        {
+            var input = InputItems(inputFile);
+            Position p = new Position();
+            foreach (var line in input)
+            {
+                var parts = line.Split();
+                int value = int.Parse(parts[1]);
+                if (parts[0] == "forward")
+                {
+                    p.PositionHorizontal += value;
+                    p.Depth += p.Aim * value;
+                }
+                if (parts[0] == "up")
+                {
+                    p.Aim -= value;
+                }
+                if (parts[0] == "down")
+                {
+                    p.Aim += value;
+                }
+            }
+            return p.Depth * p.PositionHorizontal;
+        }
+    }
+}
---
+++ dotnet/Tester/2021/Day2Test.cs
@@ -0,0 +1,37 @@
+using FluentAssertions;
+
+using NUnit.Framework;
+
+using Solver._2021;
+
+namespace Tester._2021
+{
+    public class Day2Test : TestBase
+    {
+        [SetUp]
+        public void Setup()
+        {
+            SetInput(@"forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+");
+
+            solver = new Day2(fileSystem);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            Solve1Int().Should().Be(150);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Solve2Int().Should().Be(900);
+        }
+    }
+}
