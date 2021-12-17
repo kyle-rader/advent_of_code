6ab2c8073750916f37944e2c065f9c5e55ad7351
Kyle Rader <kylewrader@gmail.com>
2021-12-17 14:58:27 -0800

    dotnet 2021 day 10

--- dotnet/Solver/2021/Day10.cs
+++ dotnet/Solver/2021/Day10.cs
@@ -4,23 +4,144 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Text;
 
 namespace Solver._2021
 {
     public class Day10 : Base
     {
+        public class NavSystem
+        {
+            public enum ParseResultState
+            {
+                Valid,
+                Incomplete,
+                Corrupt,
+            }
+
+            public struct ParseResult
+            {
+                public ParseResultState state;
+                public double ScoreCorrupt;
+                public double ScoreIncomplete;
+            }
+
+            public static HashSet<char> Openers = new HashSet<char>()
+            {
+                '(', '[', '{', '<',
+            };
+
+            public static HashSet<char> Closers = new HashSet<char>()
+            {
+                ')', ']', '}', '>',
+            };
+
+            public static readonly Dictionary<char, char> OpenerFromCloser = new Dictionary<char, char>()
+            {
+                { ')', '(' },
+                { ']', '[' },
+                { '}', '{' },
+                { '>', '<' },
+            };
+
+            public static readonly Dictionary<char, char> CloserFromOpener = new Dictionary<char, char>()
+            {
+                { '(', ')' },
+                { '[', ']' },
+                { '{', '}' },
+                { '<', '>' },
+            };
+
+            public static readonly Dictionary<char, double> PointsCorrupt = new Dictionary<char, double>()
+            {
+                { ')', 3 },
+                { ']', 57 },
+                { '}', 1197 },
+                { '>', 25137 },
+            };
+
+            public static readonly Dictionary<char, double> PointsIncomplete = new Dictionary<char, double>()
+            {
+                { ')', 1 },
+                { ']', 2 },
+                { '}', 3 },
+                { '>', 4 },
+            };
+
+            public static ParseResult Validate(string input)
+            {
+                Stack<char> stack = new Stack<char>();
+                foreach (var c in input)
+                {
+                    var isClosing = Closers.Contains(c);
+                    if (Openers.Contains(c))
+                    {
+                        stack.Push(c);
+                    }
+                    else if (isClosing && stack.Count == 0)
+                    {
+                        return new ParseResult() {
+                            state = ParseResultState.Corrupt,
+                            ScoreCorrupt = PointsCorrupt[c],
+                        };
+                    }
+                    else if (isClosing)
+                    {
+                        var opener = stack.Pop();
+                        if (opener != OpenerFromCloser[c])
+                        {
+                            return new ParseResult()
+                            {
+                                state = ParseResultState.Corrupt,
+                                ScoreCorrupt = PointsCorrupt[c],
+                            };
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException($"Unkown instruction char found '{c}'");
+                    }
+                }
+
+                var result = new ParseResult()
+                {
+                    state = ParseResultState.Valid,
+                };
+
+                if (stack.Count > 0)
+                {
+                    result.state = ParseResultState.Incomplete;
+                    while (stack.Count > 0)
+                    {
+                        result.ScoreIncomplete = (result.ScoreIncomplete * 5) + PointsIncomplete[CloserFromOpener[stack.Pop()]];
+                    }
+                }
+                return result;
+            }
+        }
+
         public Day10(IFileSystem fileSystem, string inputFile) : base(fileSystem, inputFile) { }
 
         public override double Solve()
         {
             var input = InputLines();
-            return -1;
+            return input.Aggregate(0.0, (acc, line) =>
+            {
+                var result = NavSystem.Validate(line);
+                return acc + result.ScoreCorrupt;
+            });
         }
 
         public override double Solve2()
         {
             var input = InputLines();
-            return -1;
+            var incompleteScores = input
+                .Select(NavSystem.Validate)
+                .Where(r => r.state == NavSystem.ParseResultState.Incomplete)
+                .Select(r => r.ScoreIncomplete)
+                .ToList();
+            incompleteScores.Sort();
+            return incompleteScores[incompleteScores.Count / 2];
         }
     }
 }
--- dotnet/Tester/2021/Day10Test.cs
+++ dotnet/Tester/2021/Day10Test.cs
@@ -3,6 +3,7 @@ using FluentAssertions;
 using NUnit.Framework;
 
 using Solver._2021;
+using NavSystem = Solver._2021.Day10.NavSystem;
 
 namespace Tester._2021
 {
@@ -12,21 +13,73 @@ namespace Tester._2021
         public void Setup()
         {
             SetInput(@"
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
 ");
 
             solver = new Day10(fileSystem, INPUT_FILE);
         }
 
+        [TestCase("()")]
+        [TestCase("[]")]
+        [TestCase("{}")]
+        [TestCase("<>")]
+        [TestCase("(<>)")]
+        [TestCase("[(<>)]")]
+        [TestCase("{[(<>)()<>]}")]
+        [TestCase("[][](<>)")]
+        public void NavSystem_Validate_Valid(string input)
+        {
+            NavSystem.Validate(input).state.Should().Be(NavSystem.ParseResultState.Valid);
+        }
+
+        [TestCase("(")]
+        [TestCase("[")]
+        [TestCase("{")]
+        [TestCase("<")]
+        [TestCase("({[")]
+        [TestCase("({[]")]
+        [TestCase("({[]}")]
+        [TestCase("({[]})(")]
+        public void NavSystem_Validate_Incomplete(string input)
+        {
+            NavSystem.Validate(input).state.Should().Be(NavSystem.ParseResultState.Incomplete);
+        }
+
+        [TestCase("(]")]
+        [TestCase("[)")]
+        [TestCase("{>")]
+        [TestCase("<}")]
+        [TestCase("([]>")]
+        public void NavSystem_Validate_Corrupt(string input)
+        {
+            NavSystem.Validate(input).state.Should().Be(NavSystem.ParseResultState.Corrupt);
+        }
+
+        [TestCase("({}))")]
+        public void NavSystem_Validate_TooManyClosers(string input)
+        {
+            NavSystem.Validate(input).state.Should().Be(NavSystem.ParseResultState.Corrupt);
+        }
+
         [Test]
         public void Part1()
         {
-            Solve1Int().Should().Be(0);
+            Solve1Double().Should().Be(26397);
         }
 
         [Test]
         public void Part2()
         {
-            Solve2Int().Should().Be(0);
+            Solve2Double().Should().Be(288957);
         }
     }
 }
