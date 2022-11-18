2cf2cae09ca6648cdc07b15a9d9a782901cece6a
Kyle Rader <kylewrader@gmail.com>
2022-11-18 00:55:50 -0800

    Move to 1 crate with modules for year and day

---
+++ src/rust/src/y2015/d02.rs
@@ -0,0 +1,22 @@
+fn part1(input: &str) -> usize {
+    todo!()
+}
+
+fn part2(input: &str) -> usize {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("foobar"), 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("foobar"), 0);
+    }
+}
