2b1192ced52d1b0a63e6ce16d3b63f1420e46ca5
Kyle Rader <kylewrader@gmail.com>
2022-11-20 04:37:56 -0800

    Get firefox cookies path

--- src/rust/aoc/src/login.rs
+++ src/rust/aoc/src/login.rs
@@ -1,8 +1,3 @@
-use std::path::PathBuf;
-
-use anyhow::anyhow;
-use anyhow::Result;
-
 pub fn login(token: Option<String>) -> anyhow::Result<()> {
     // Get
     let token = token.unwrap_or(token_from_cookies()?);
@@ -14,23 +9,4 @@ pub fn login(token: Option<String>) -> anyhow::Result<()> {
 
 fn token_from_cookies() -> anyhow::Result<String> {
     Ok(String::from("foobar"))
-}
-
-fn mozilla_profiles() -> anyhow::Result<Vec<PathBuf>> {
-    if let Some(mut config) = dirs::config_dir() {
-        config.push("Mozilla");
-        config.push("Firefox");
-        config.push("Profiles");
-    } else {
-        Err(anyhow!("could not get mozilla config path"))
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    #[cfg(target_os = "windows")]
-    fn mozilla_dir_exists() {}
 }
