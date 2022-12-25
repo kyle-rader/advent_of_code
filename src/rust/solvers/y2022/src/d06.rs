9f35cdd958819a65e8ed76e64559891e252477fe
Kyle Rader <kylewrader@gmail.com>
2022-12-25 06:00:39 -0800

    2022 06 parts 1+2

---
+++ src/rust/solvers/y2022/src/d06.rs
@@ -0,0 +1,141 @@
+use std::collections::{hash_map::Entry, HashMap, HashSet, VecDeque};
+
+pub struct CharCounter {
+    set: HashMap<char, u32>,
+}
+
+impl CharCounter {
+    pub fn new() -> Self {
+        Self {
+            set: HashMap::new(),
+        }
+    }
+
+    pub fn add(&mut self, c: char) {
+        self.set.entry(c).and_modify(|c| *c += 1).or_insert(1);
+    }
+
+    pub fn remove(&mut self, c: char) {
+        let value = self.set.entry(c).and_modify(|c| *c -= 1).or_default();
+        if *value == 0 {
+            self.set.remove(&c);
+        }
+    }
+
+    pub fn get(&self, c: &char) -> Option<&u32> {
+        self.set.get(c)
+    }
+
+    pub fn len(&self) -> usize {
+        self.set.len()
+    }
+}
+
+fn marker_finder(s: &str, n: usize) -> Result<usize, String> {
+    let preamble = n
+        .checked_sub(1)
+        .ok_or_else(|| String::from("n must greater than 1"))?;
+    let mut counter = CharCounter::new();
+    let mut last = VecDeque::with_capacity(n);
+
+    s.chars().take(preamble).for_each(|c| {
+        counter.add(c);
+        last.push_front(c);
+    });
+
+    for (i, c) in s.chars().skip(preamble).enumerate() {
+        counter.add(c);
+        last.push_front(c);
+        if counter.len() == n {
+            return Ok(i + n);
+        }
+        // Remove last, and 4th will be added next iteration
+        if let Some(to_remove) = last.pop_back() {
+            counter.remove(to_remove);
+        } else {
+            return Err(format!("Execpted to remove the last char in the sliding window on iteration {i} but got None"));
+        }
+    }
+    Err(String::from("Did not find marker!"))
+}
+
+#[allow(dead_code)]
+fn part1(input: &str) -> Result<usize, String> {
+    marker_finder(input, 4)
+}
+
+#[allow(dead_code)]
+fn part2(input: &str) -> Result<usize, String> {
+    marker_finder(input, 14)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod packet_finder {
+        use super::*;
+
+        #[test]
+        fn can_add() {
+            let mut subject = CharCounter::new();
+            subject.add('c');
+            assert_eq!(subject.get(&'c'), Some(&1));
+        }
+
+        #[test]
+        fn can_add_many() {
+            let mut subject = CharCounter::new();
+            (0..10).for_each(|_| {
+                subject.add('c');
+            });
+            assert_eq!(subject.get(&'c'), Some(&10));
+        }
+
+        #[test]
+        fn has_size() {
+            let mut subject = CharCounter::new();
+            (0..10).for_each(|_| {
+                subject.add('c');
+            });
+            subject.add('b');
+            assert_eq!(subject.len(), 2);
+        }
+
+        #[test]
+        fn can_remove() {
+            let mut subject = CharCounter::new();
+            subject.add('a');
+            subject.add('b');
+            subject.add('c');
+            subject.add('b');
+            assert_eq!(subject.len(), 3);
+            subject.remove('b');
+            assert_eq!(subject.len(), 3);
+            subject.remove('b');
+            assert_eq!(subject.len(), 2);
+        }
+    }
+
+    #[test]
+    fn example_input_part1() {
+        assert_eq!(part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), Ok(7));
+        assert_eq!(part1("nppdvjthqldpwncqszvftbrmjlhg"), Ok(6));
+        assert_eq!(part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), Ok(10));
+        assert_eq!(part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), Ok(11));
+    }
+
+    #[test]
+    fn part1_works() {
+        assert_eq!(part1(INPUT), Ok(1093));
+    }
+
+    #[test]
+    fn part2_works() {
+        assert_eq!(part2(INPUT), Ok(42));
+    }
+}
+
+#[cfg(test)]
+const INPUT: &str = "cvtvbvfbvfbfqfrrtnnmqqmcqqpfplfpphchpcpcfppmwwhhscccjwjnnctcjtjgjbgjjdzjjwzwfffnhhgppjmpmgppppzbzbrzznttfqqhtqqbssrjjffgssnqnsqnsqnnmlmmztmtqmqccqscsshrsrtstrrjwrrswwpnnftfwwwzznsstbbbtzzncnbcnbccwtwctwwjgjcjtccblclgcgjgngwgbggpbbbbbgcczzfsfbbwlwwnqwnwbnwnmmbjjnfnqfqfvvgvqqvcvggpnpssptpnpjjvvpdpqpcqpcqqnhhnqncqclcfcfjcjhjdjqddhndnmmvmfmsmdsdwsdswsnsgsmsrmrjjpzzzzcppczcscsgcczqcqnnpzpbbdqqqqsqlslzljzjdjbjdjvvlhvvnbbjtbbqbdqdggzwgghhdssmttjtttmwtmtjjcpjpjptjpjmpptnntgttjpttljjwtjwwrgrvgrvrmvvrvdrdcdmmvvbpbddfbdbppdccjdjvjpjjgzjzdzfddqcqlccbvbqvqvjqjggqrqdrqrhhwggmvgmgvgrvvzwzddlzlbbdpbdbtbhthnthttjwtwffwvvrccnpcnclcgcrrgdrrgzgmzggzhhftfppnwppqhhmddzhdzzzjqjhqqcgqqmjjwppfrprlrjlrjjlhjlhjjnbnlblbzlbbfjjwfjjdcdndvvbllfmlmfmvmnnlcnncsnslsvlsvlsltslslqsqzssslqlssqrrpdpwdppsfpsprpffchhcvcpvvjpjvjmvvrjrqrzqqqlvvzzggcgrcgcmmvnmmchmmswsvwsvwswnwswdwmwzmmqfqrqtrqqwqlqlmqmsmggpzpjjlvllzplzlbzzbhhglhhwwhfwwpwgwrwlwplppfvflftltffccmcrcgrcgghgmmbrbppwvvghvhphhztzpzzwbbtpppcnpccnvcvbccvfvmffmtmbttdqdrrfnrnffsgstttbhhgmmtffhthllsbllsqsllptpwpvpdpsddsgsrrngnpggwbwwppfqfmqfmfvfzzzldltdtvdvtvtbvtgpvtnvcsdbwjrqwrfzdhwfwnmtrltdhsfzwwfpscrlsffchnfszqdddsztwzcbwdwfnljpfdsqgpnjffqfgsprggfwgvfldvzmnrpnzvfscswnzbljtvshzmcztqqwtzdstsqggqqvvlmspjjcmllcmsndwpllrvlrglphcqjgwzntwqffthrfmfgpcvjvclvdrjrpnwtrlshlnwgbbznpflsfmgvbwmnwjpnqhmdnsmwnmblqzjfzfcslhntnjhbrvvlfbffsbhrtvjltqrqszsrpmdghmhsrqlmrjdsgtsgwggdlqptqrvgmwrpcjrnmmmcdpgjzgghqmshhmddfvfrqwnwvwbvzzpmwzmvvgsqlsgvmtczwnbzrtnzjnpjjghdzsmcgvqpdhltvltbfvgsgzhvqjjnpspngqpvqwvtqrczqzwrclqbnplmvlnvcwdbzlrnwbppvdfzmblnqfpfqlngttrrcjzfjnrnmssdrrvbvhtrfzblzsjbqwbttsshmcmfhrfbfqspvcfmqbwjszmvwqnpszcfhtnqtftvtpvcjrcnlmfnvlpjzspcnmvvshpsdfhtbhgjcmjmtpmtcbwnlgtnvlwlggbnzfrrqljmjrwjtzfmgbllmmjtfhrszfrfpftmpzhzdhfwdbdnwqspjbrlglgdwtfzcrdhwgqltvthbnrqfngjgnddqlcpjflqzvbhmlzsvpvlzdvhspmchrjlzfgnlpwdnszqdqpnrchdrswfwnflslcpfmbfrplsmchbgjhhhlzjlvztpdhslrpmmcthhjwfczjtmbmsvzvsqrvzrcnflqldfnhjzlcsfhgrtqtscnbzqjglgjnqbtprrprrdtqqtlqwdqnmgbglvwtrpnpjqdsmvczbnldvmgmtpmmqbwgjbfsqbjvnjhhlrppptlrjsptpvghbgtvwvsrnggznjsbjpnwpdsqmmjjqjdwshqwzvqgpnlhdldwzmvmdnhrnhpscrbnbdtmfqwlmrlszljpjdqgjpgcdwtqzsbqsndjdslrstjwbbfnjvrjlbrphqspltjzpvsgccfrmqhfhjmlslncpgpflhjlrzqsffrtcgsvzpjmlqjznscgjrjhzfjbbchvgjgvfsrpnzdrstdtsltqldfldvfdjqdwsltcbnnvcgvnjpgdjzsbzgfvgvtmghwfmrmblscfdqlsdgcdbgsphfvvvnffcqwlrvnsrjmvvdrjdljlppsfsrmwwvmsqcgtgtfjhqbqvlsnwdwzhncgprbspjzqnbllwcjnqpfzlhscdtvlldffngwttntsbtlctdfbzjgclgbhhjtlffvtgbdsswczlgmwntrrnnbdtflplfbtqcfrhtgjnrltvzqlcvtdthldqrvjdmlntdjzcncrplgmdbqhlrrfdszrnbrllhgpzzgjnqzrjrtjjntqvdphgvjtbtqfjwvlpprrcvmpjcdmntvtqjvjrhngbtmdtflpqmlmmvjjjpdlcvntlvlgstbblwfwjrbcqjgzvjjzflfhdvgnhpnqprcvmhsrpmgfcgjpdbqpmpvlnmzdngzqcqtdsbbqbdhtgpqmqwtcctsvhwmlhpgzgpwwsqngsrwzghfhzbqcpspfhqbllbvpwgwdnghqnvjtfqtwgvnbfbpcffbwwbfqgqnftpqdtwhmrprpgdlnwrcmdndllwbvlrrjvmwlbgfwgbhtmbqhfvhlhlthbrljwnwcgsdgsbbzgnwzmjfgnfjcqhspjwctntznjptsnbpwtfzmrqwjzrmdthfwdvttrcghbjndfmtswhsmdwwwffmgsvshmlhdbwdscjnvrvqnwttlsmfbssnsscftcrpwfbbplbpmvzmttvbgpnjtqqzcfdfwqdphggwbnhfvssmdjvbvtlrcghltthqrqdbvjnbqpzjcggrbnrswhvdcrmgncsmhsmstmcbwgbfwvzqtdrpjbgljdgcncgcdslhftptnvfhgpsjqqmnsjmqhztnfzqqqhccbnqjlhqcwwbfzgpmbvjszswjlhcqshtqdmwmhpgqgzbwbtchrdwgnvfqcgcmngtddpzfgjrrssdmcnbtpbhfvfbdgzmcbjqtdqrnrdchtrcfpmtnvfczdlnjplwccfmzjwwvdsrcjznrcbczdqwshjgvhjpgdgjvtvmdzncrqgffzhcbwstvltqbrzfbnrrvlntjmzctvnqhdcgvvmmbfplzpwgsdzdtvnwnshnwbwqplbctntslczwhblsnzdcsgltvlfvbtvthvlsmzmfgzhpvrvczdhmcpmgqzbdhbcdbstrrtzgrbhhwwghzlgcdtlcjtjdcwdtqpvpwzrwmhczfsdmwjlgtmzvdbwzdnsztvqzhjpfrqzllgvrsbmhvrlzwtfmpzchftrzlmndpwqgcmhdzmjrjvpbnrcmjvzbzlqngmrzcbdwfcwmlnlqsdgbvmvqcsjtwgvtpmqdtdqfrshmlgnjqdwsrpjcfnfpjzlhslsvppmgprqlpcfztcngmqgrvqvpcszzgtbpcrghmgnvgplctgdljhwjgbzmjrdsccdtrwjrrbttzmpvmzlmqdfwdccfzqztdjmcgrcdwghgmmntdwrclshlgnsmhmltwcczncndtglnqvcvbwlvcgqtzpnqnlllchwjwrlhcdlbjlzvhvtjsttqhvhfqbsqjpfcmpsfshgrrwgprjtfnhsjqqntrjfgjjmjbqwdjlcjjdljpppgvqvgrrwvclbmncmqglhbtjcfrbjmhqgmlpzgljntqvrcggmrgtbrftvhrnjbvzstlnzrqcgncnjhdrnqcnwcrstvdsgnwzjzmqsdtwzzjmfgzpslhqclhtjnrlwwfrqgmhsttfdprhgmphzfdfqntwztltnnfqwgdnnbrlgzmtwqsvhbjjqtcgmghnbchdfgfrdtjnvlgtqdgrfwmgnmpslqmqzdgqztnppbc
+";
