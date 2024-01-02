8bb8824d9cf2a15d2fac3804e86bf1bfb02698e1
Kyle Rader <kylewrader@gmail.com>
2024-01-01 16:46:26 -0800

    rust 2023: day 4 part 2

--- src/rust/solvers/y2023/src/d04.rs
+++ src/rust/solvers/y2023/src/d04.rs
@@ -1,26 +1,109 @@
+use std::collections::{HashSet, VecDeque};
+
 #[allow(dead_code)]
 fn part1(input: &str) -> Result<u64, String> {
-    Ok(0)
+    Ok(input.lines().map(score_card).sum::<u64>())
+}
+
+fn parse_card(line: &str) -> (HashSet<u64>, HashSet<u64>) {
+    let mut parts = line.split('|');
+    let winning = parts
+        .next()
+        .unwrap()
+        .split(':')
+        .nth(1)
+        .unwrap()
+        .split_whitespace()
+        .map(|x| x.parse::<u64>().unwrap());
+    let winning = HashSet::from_iter(winning);
+    let have = parts
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .map(|x| x.parse::<u64>().unwrap());
+    let have = HashSet::from_iter(have);
+    (winning, have)
+}
+
+fn score_card(line: &str) -> u64 {
+    let (winning, have) = parse_card(line);
+    match winning.intersection(&have).count() {
+        0 => 0,
+        n => 2_u64.pow(n as u32 - 1),
+    }
 }
 
 #[allow(dead_code)]
 fn part2(input: &str) -> Result<u64, String> {
-    Ok(0)
+    let card_scores: Vec<usize> = input
+        .lines()
+        .map(parse_card)
+        .map(|(winning, have)| winning.intersection(&have).count())
+        .collect();
+    let mut total = card_scores.len() as u64;
+    let mut queue: VecDeque<usize> = (0..card_scores.len()).collect();
+
+    while !queue.is_empty() {
+        let card = queue.pop_front().unwrap();
+        let score = card_scores[card];
+        let cards_won: Vec<usize> = (card..=(card + score as usize)).skip(1).collect();
+
+        cards_won.iter().for_each(|c| queue.push_back(*c));
+        total += score as u64;
+    }
+
+    Ok(total)
 }
 
 #[cfg(test)]
 mod tests_y2023 {
     use super::*;
+    use test_case::test_case;
 
     const INPUT: &str = include_str!("d04.txt");
 
+    const EXAMPLE: &str = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+    #[test]
+    fn example_part1() {
+        assert_eq!(part1(EXAMPLE), Ok(13));
+    }
+
+    #[test]
+    fn example_part2() {
+        assert_eq!(part2(EXAMPLE), Ok(30));
+    }
+
+    #[test_case("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", 8)]
+    #[test_case("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", 2)]
+    #[test_case("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1", 2)]
+    #[test_case("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83", 1)]
+    #[test_case("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36", 0)]
+    #[test_case("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", 0)]
+    fn test_score_card(line: &str, expected: u64) {
+        assert_eq!(score_card(line), expected);
+    }
+
+    #[test]
+    fn parse_line() {
+        let line = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
+        let (winning, have) = parse_card(line);
+        assert_eq!(winning, HashSet::from_iter(vec![41, 48, 83, 86, 17]));
+        assert_eq!(have, HashSet::from_iter(vec![83, 86, 6, 31, 17, 9, 48, 53]));
+    }
+
     #[test]
     fn part1_works() {
-        assert_eq!(part1(INPUT), Ok(42));
+        assert_eq!(part1(INPUT), Ok(25183));
     }
 
     #[test]
     fn part2_works() {
-        assert_eq!(part2(INPUT), Ok(42));
+        assert_eq!(part2(INPUT), Ok(5667240));
     }
 }
