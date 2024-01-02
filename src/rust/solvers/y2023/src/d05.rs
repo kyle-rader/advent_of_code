4a196e083cee1e192f92c9ed43242933c46f15b7
Kyle Rader <kylewrader@gmail.com>
2024-01-02 00:53:29 -0800

    rust 2023 day 5

--- src/rust/solvers/y2023/src/d05.rs
+++ src/rust/solvers/y2023/src/d05.rs
@@ -1,10 +1,35 @@
-use std::{num::ParseIntError, str::FromStr};
+use std::{collections::HashMap, num::ParseIntError, str::FromStr};
 
 use thiserror::Error;
 
 #[allow(dead_code)]
 fn part1(input: &str) -> Result<u64, String> {
-    Ok(0)
+    let mut inputs = input.split("\n\n");
+    let seeds = parse_seeds(inputs.next().unwrap());
+    let maps = inputs
+        .map(|s| s.parse::<Map>().map_err(|e| e.to_string()))
+        .collect::<Result<Vec<Map>, _>>()
+        .map_err(|e| e.to_string())?;
+    let maps: HashMap<Category, Map> = maps.into_iter().map(|m| (m.source, m)).collect();
+
+    let locations: Vec<u64> = seeds
+        .iter()
+        .map(|seed| {
+            let mut val = *seed;
+            let mut category = Some(Category::Seed);
+            while category.is_some() {
+                if let Some(map) = maps.get(&category.unwrap()) {
+                    val = map.transform(val);
+                    category = Some(map.target);
+                } else {
+                    category = None;
+                }
+            }
+            val
+        })
+        .collect();
+
+    Ok(*locations.iter().min().unwrap())
 }
 
 #[allow(dead_code)]
@@ -20,6 +45,7 @@ fn parse_seeds(input: &str) -> Vec<u64> {
         .collect()
 }
 
+#[derive(Debug, PartialEq, Eq)]
 struct MapRange {
     from: u64,
     to: u64,
@@ -28,14 +54,54 @@ struct MapRange {
 
 impl MapRange {
     pub fn contains(&self, value: u64) -> bool {
-        value >= self.from && value <= self.from + self.length
+        value >= self.from && value < (self.from + self.length)
     }
 }
 
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
+enum Category {
+    Seed,
+    Soil,
+    Fertilizer,
+    Water,
+    Light,
+    Temperature,
+    Humidity,
+    Location,
+}
+
+impl From<&str> for Category {
+    fn from(s: &str) -> Self {
+        match s {
+            "seed" => Category::Seed,
+            "soil" => Category::Soil,
+            "fertilizer" => Category::Fertilizer,
+            "water" => Category::Water,
+            "light" => Category::Light,
+            "temperature" => Category::Temperature,
+            "humidity" => Category::Humidity,
+            "location" => Category::Location,
+            _ => panic!("Unknown category: {}", s),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
 struct Map {
-    source: String,
-    target: String,
+    source: Category,
+    target: Category,
     ranges: Vec<MapRange>,
+}
+
+impl Map {
+    pub fn transform(&self, value: u64) -> u64 {
+        if let Some(range) = self.ranges.iter().find(|range| range.contains(value)) {
+            let offset = value - range.from;
+            range.to + offset
+        } else {
+            value
+        }
+    }
 }
 
 #[derive(Debug, Error, PartialEq, Eq)]
@@ -130,49 +196,58 @@ humidity-to-location map:
 
         let subject: Map = map.parse().unwrap();
         let expected = Map {
-            source: "fertilizer".to_string(),
-            target: "water".to_string(),
+            source: Category::Fertilizer,
+            target: Category::Water,
             ranges: vec![
                 MapRange {
-                    from: 49,
-                    to: 53,
+                    from: 53,
+                    to: 49,
                     length: 8,
                 },
                 MapRange {
-                    from: 0,
-                    to: 11,
+                    from: 11,
+                    to: 0,
                     length: 42,
                 },
                 MapRange {
-                    from: 42,
-                    to: 0,
+                    from: 0,
+                    to: 42,
                     length: 7,
                 },
                 MapRange {
-                    from: 57,
-                    to: 7,
+                    from: 7,
+                    to: 57,
                     length: 4,
                 },
             ],
         };
+
+        assert_eq!(subject, expected);
     }
 
     #[test]
-    fn test_map_rang_contains() {
+    fn test_map_range_contains() {
         let range = MapRange {
             from: 0,
             to: 10,
             length: 10,
         };
+
         for i in 0..10 {
             assert!(range.contains(i))
         }
         assert!(!range.contains(10));
+        assert!(!range.contains(11));
+    }
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part1(EXAMPLE), Ok(35));
     }
 
     #[test]
     fn part1_works() {
-        assert_eq!(part1(INPUT), Ok(42));
+        assert_eq!(part1(INPUT), Ok(388071289));
     }
 
     #[test]
